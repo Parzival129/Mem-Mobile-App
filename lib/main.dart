6e1d6b8e354ad5361fdca017d04bb932ec10502e
Russell Tabata <Russell@macbook-pro.lan>
2021-09-09 16:45:28 -0400

    new icon & color

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'SPARQ',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       home: MyHomePage(title: 'Sparq Home Page'),
     );
