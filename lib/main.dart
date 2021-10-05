53bb3616858bd88b74f1babf90930afd6be55dd7
Russell Tabata <Russell@eva.lan>
2021-10-05 16:16:13 -0400

    updated the ui abit

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'SPARQ',
       theme: ThemeData(
+        brightness: Brightness.light,
         primarySwatch: Colors.red,
+        textTheme: const TextTheme(
+          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          headline6: TextStyle(fontSize: 36.0),
+          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        ),
       ),
-      home: MyHomePage(title: 'Sparq Home Page'),
+      home: MyHomePage(title: "SPARQ"),
     );
   }
 }
